338d620abba2bb54d1a02555bff44c15d1633c75
riverplant <riverplant@hotmail.com>
2024-10-09 14:16:29 -0400

    first commit

---
+++ src/main/java/com/example/ThinkOnDemo/ThinkOnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThinkOnDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkOnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkOnDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThinkOnDemo/ThinkOnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThinkOnDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkOnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
